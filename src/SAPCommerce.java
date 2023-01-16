7a09caa9b7a3fd0ed9d23a58b5899c75c64709b5
Pedro Leonardo <pedro.leonardo7738@gmail.com>
2023-01-16 10:48:07 -0300

    SAPCommerce

---
+++ src/SAPCommerce.java
@@ -0,0 +1,8 @@
+public class SAPCommerce {
+    public void addtoCart(){
+        System.out.println("Adicionando no carrinho!");
+    }
+    public void removeFromCart(){
+        System.out.println("Removendo do carrinho!");
+    }
+}
---
+++ src/TesteSAPCommerce.java
@@ -0,0 +1,8 @@
+public class TesteSAPCommerce {
+    public static void main(String[] args) {
+        SAPCommerce sapCommerce = new SAPCommerce();
+
+        sapCommerce.addtoCart();
+        sapCommerce.removeFromCart();
+    }
+}
